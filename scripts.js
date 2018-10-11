95b7b134e5bd15c17350177a93a4cd1077a609f5
Matthew Campbell <cattmampbell@gmail.com>
2018-10-11 19:58:02 -0400

    Connected index.hthml and scripts.js files via <src> tag.

--- scripts.js
+++ scripts.js
@@ -1,35 +1,52 @@
 document.addEventListener(`DOMContentLoaded`, () => {
     // Define/assign all variables/functions:
+    // Top Row
     const TOP_LEFT_SQUARE = document.querySelector(`#squareA`);
     const TOP_MIDDLE_SQUARE = document.querySelector(`#squareB`);
     const TOP_RIGHT_SQUARE = document.querySelector(`#squareC`);
+    // Middle Row
     const MIDDLE_LEFT_SQUARE = document.querySelector(`#squareD`);
     const MIDDLE_CENTER_SQUARE = document.querySelector(`#squareE`);
     const MIDDLE_RIGHT_SQUARE = document.querySelector(`#squareF`);
+    // Bottom
     const BOTTOM_LEFT_SQUARE = document.querySelector(`#squareG`);
     const BOTTOM_MIDDLE_SQUARE = document.querySelector(`#squareH`);
     const BOTTOM_RIGHT_SQUARE = document.querySelector(`#squareI`);
-    const SQUARE = document.querySelector(`.square`);
+    const BOARD = document.getElementById(`board`);
     let playerMove = 0; 
 
-    const IS_ODD = (moveNumber) => { 
-        return moveNumber & 1; // True
+    // Creates/appends <elementName id="idName" class="className">, returns element
+    const CREATE_ELEMENT = (elementName, idName, className, appendTo) => {
+        const ELEMENT = document.createElement(elementName);
+        ELEMENT.id = idName;
+        ELEMENT.className = className;
+        appendTo.appendChild(ELEMENT);
+        return ELEMENT;
     }
 
-    const IS_EVEN = (moveNumber) => {
-        return !(moveNumber & 1); // True
+    // Creates/appends <appendTo>textContent, returns element
+    const CREATE_TEXT_NODE = (textContent, appendTo) => {
+        const ELEMENT = document.createTextNode(textContent);
+        appendTo.appendChild(ELEMENT);
+        return ELEMENT;
     }
 
-    SQUARE.addEventListener(`click`, (event) => {
+    BOARD.addEventListener(`click`, (event) => {
         playerMove += 1; // Adds 1 to playerMove
-        console.log(playerMove);
+        const CLICKED_SQUARE = event.target;
 
-        // if(IS_ODD(playerMove) === true) {
-        //     event.target.style.color = `#de3241`; // Red
-        //     event.target.textContent = `X`;
-        // } else if(IS_EVEN(playerMove) === true) {
-        //     event.target.style.color = `#0586ff`; // Blue
-        //     event.target.textContent = `O`;
-        // }
+        if(CLICKED_SQUARE.)
+
+        if(playerMove % 2 === 0) { // Even
+            const CLICKED_O_SQUARE = event.target;
+            const O_SQUARE = CREATE_ELEMENT(`p`, `moveNumber${playerMove}`, `squareTxt`, CLICKED_O_SQUARE);
+            const O_SQUARE_TEXT = CREATE_TEXT_NODE(`O`, O_SQUARE);
+            O_SQUARE.style.color = `#0586ff`; // Blue
+        } else { // Odd
+            const CLICKED_X_SQUARE = event.target;
+            const X_SQUARE = CREATE_ELEMENT(`p`, `moveNumber${playerMove}`, `squareTxt`, CLICKED_X_SQUARE);
+            const X_SQUARE_TEXT = CREATE_TEXT_NODE(`X`, X_SQUARE);
+            X_SQUARE.style.color = `#de3241`; // Red
+        }
     })
 })
