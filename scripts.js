368e3f8b391f8bfc7aa1a62ab3bb73cd66573537
Matthew Campbell <cattmampbell@gmail.com>
2018-10-13 13:57:02 -0400

    Formatted code in scripts.js file to make it more readable.

--- scripts.js
+++ scripts.js
@@ -20,32 +20,32 @@ function setTurnMessage(message) {
 function switchTurn() {
     // If checkWinner returns true
     if (checkWinner(document.turn)) {
-        // Displays, "Congrats ___'s, you are the winner!", message in #turnMessage via setTurnMessage()
-        setTurnMessage('Congrats ' + document.turn + '\'s, you are the winner!');
+        // Displays, "Congratulations ___'s, you are the winner!", message in #turnMessage via setTurnMessage()
+        setTurnMessage('Congratulations ' + document.turn + '\'s, you are the winner!');
 
         // Set document.turn 'color:' to '#28a745;' (#28a745 = 'success' (green) in Bootstrap 4.0)
         document.getElementById('turnMessage').style.color = '#28a745';
 
-        // Else if document.turn is 'X'    
+    // Else if document.turn is 'X'    
     } else if (document.turn == 'X') {
         // Set document.turn to 'O'
         document.turn = 'O';
 
-        // Display, "It's ___'s turn.", message in #turnMessage via setTurnMessage()
-        setTurnMessage('It\'s ' + document.turn + '\'s turn.');
+        // Display, "It is O's turn.", message in #turnMessage via setTurnMessage()
+        setTurnMessage('It is ' + document.turn + '\'s turn');
 
         // Set document.turn 'color:' to '#0586ff;' (#0586ff = 'primary' (blue) in Bootstrap 4.0)
         document.getElementById('turnMessage').style.color = '#0586ff';
-        // Else...
+    // Else...
     } else {
         // Set document.turn to 'X'
         document.turn = 'X';
 
-        // Display, "It's ___'s turn.", message in #turnMessage via setTurnMessage()
-        setTurnMessage('It\'s ' + document.turn + '\'s turn.');
+        // Display, "It's X's turn.", message in #turnMessage via setTurnMessage()
+        setTurnMessage('It is ' + document.turn + '\'s turn.');
 
         // Set document.turn 'color:' to '#de3241;' (#de3241 = 'danger' (red) in Bootstrap 4.0)
-        document.getElementById('turnMessage').style.color = '#de3241'; // Red
+        document.getElementById('turnMessage').style.color = '#de3241'; 
     }
 }
 
@@ -58,10 +58,13 @@ function nextMove(square) {
 
         // Call switchTurn() 
         switchTurn();
-        // Else the .square isn't empty
+    // Else the .square isn't empty
     } else {
         // Display, "Oops, pick a different square", message in #turnMessage via setTurnMessage()
-        setTurnMessage('Oops, pick a different square.');
+        setTurnMessage('Oops, pick a different square!');
+
+        // Set document.turn 'color:' to '#ffc904;' (#ffc904 = 'warning' (yellow) in Bootstrap 4.0)
+        document.getElementById('turnMessage').style.color = '#ffc904'; 
     }
 }
 
@@ -75,23 +78,27 @@ function checkWinner(move) {
     // Initialize result to false
     let result = false;
 
-    // If...
+    /*
+    *   #square1   |   #square2   |   #square3
+    *   #square4   |   #square5   |   #square6
+    *   #square7   |   #square8   |   #square9
+    */
     if (
-        // ...top left to top right/#square1, #square2 and #square3 = 'X', 'X', 'X'/'O', 'O', 'O'
+        // ...#square1, #square2 and #square3 = 'X', 'X', 'X'/'O', 'O', 'O'
         checkRow(1, 2, 3, move) ||
-        // ...middle left to middle right/#square4, #square5 and #square6 = 'X', 'X', 'X'/'O', 'O', 'O'
+        // ...#square4, #square5 and #square6 = 'X', 'X', 'X'/'O', 'O', 'O'
         checkRow(4, 5, 6, move) ||
-        // ...bottom left to bottom right/#square7, #square8 and #square9 = 'X', 'X', 'X'/'O', 'O', 'O'
+        // ...#square7, #square8 and #square9 = 'X', 'X', 'X'/'O', 'O', 'O'
         checkRow(7, 8, 9, move) ||
-        // ...top left to bottom left/#square1, #square4 and #square7 = 'X', 'X', 'X'/'O', 'O', 'O'
+        // ...#square1, #square4 and #square7 = 'X', 'X', 'X'/'O', 'O', 'O'
         checkRow(1, 4, 7, move) ||
-        // ...top middle to bottom middle/#square2, #square5 and #square8 = 'X', 'X', 'X'/'O', 'O', 'O'
+        // ...#square2, #square5 and #square8 = 'X', 'X', 'X'/'O', 'O', 'O'
         checkRow(2, 5, 8, move) ||
-        // ...top right to bottom right/#square3, #square6 and #square9 = 'X', 'X', 'X'/'O', 'O', 'O'
+        // ...#square3, #square6 and #square9 = 'X', 'X', 'X'/'O', 'O', 'O'
         checkRow(3, 6, 9, move) ||
-        // ...top left to bottom right diagonal/#square1, #square5 and #square7 = 'X', 'X', 'X'/'O', 'O', 'O'
-        checkRow(1, 5, 7, move) ||
-        // ...top right to bottom left diagonal/#square3, #square5 and #square7 = 'X', 'X', 'X'/'O', 'O', 'O'
+        // ...#square1, #square5 and #square7 = 'X', 'X', 'X'/'O', 'O', 'O'
+        checkRow(1, 5, 9, move) ||
+        // ...#square3, #square5 and #square7 = 'X', 'X', 'X'/'O', 'O', 'O'
         checkRow(3, 5, 7, move)) {
         // Set result = true
         result = true;
